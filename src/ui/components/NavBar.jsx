e19cee04f868227f69de19b0d8bb3d37e75ec111
Beto <robledo.mdp@gmail.com>
2022-08-05 16:05:51 -0300

    clase 21

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,7 +1,20 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
+// TODO: Primera Tarea: En el componente Navbar.jsx agregar la clase active utilizando el callback que ofrecen los NavLinks de react-router-dom en su atributo className.
 
 export const Navbar = () => {
+
+    const navigate = useNavigate()
+
+    const isActive = ({isActive}) => `nav-item nav-link ${ isActive ? 'active' : ''}`
+    
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        })
+    }
+        
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -16,29 +29,32 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink 
-                        className="nav-item nav-link" 
+                        className={isActive} 
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink 
-                        className="nav-item nav-link" 
+                        className={isActive} 
                         to="/dc"
                     >
                         DC
                     </NavLink>
                 </div>
             </div>
-
+ 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/login"
-                    >
+                   <span className="nav-item nav-link text-primary">
+                        Beto 
+                   </span>
+                   <button 
+                        className='btn nav-item nav-link'
+                        onClick={onLogout}
+                   > 
                         Logout
-                    </NavLink>
+                   </button>
                 </ul>
             </div>
         </nav>
